ed7ef7218ce3e94525aa9ecde7dd141e0cde04ad
ArturVargas <arturvargas9226@gmail.com>
2019-07-29 21:35:57 -0500

    GraphQl

---
+++ src/resolvers/Querys.js
@@ -0,0 +1,13 @@
+
+const Post = require('../models/Post');
+
+const getAllPosts = async (root, args) => {
+    let posts = await Post.find();
+    return posts;
+ }
+
+
+
+ module.exports = {
+     getAllPosts
+ }
